a09846a787aff769f210dd9fd715bf278d2fdf32
Kyle DAmato <kyledamato4@gmail.com>
2022-05-14 13:15:14 -0400

    finished main assignment. Still need to do bonus

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 class Casino {
-    // Write code here
-};
+    constructor(name, isfakeCoin) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isfakeCoin = isfakeCoin;
+    }
 
-// TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+    playGame(betAmount) {
+        this.timesPlayed += 1;
+        if (Math.random() > 0.5 && this.fakeCoin === false) {
+            console.log("You win " + betAmount * this.timesPlayed);
+        } else {
+            console.log(this.name + "Wins!");
+        }
+    }
+}
+
+let casino = new Casino("HackerU Casino");
+casino.playGame(5);
+casino.playGame(15);
+casino.playGame(25);
+casino.playGame(35);
 
 // BONUS TESTS
 /*
